767391e57be66f860d11f0d7750d0da8ba415828
Augusto Souza <augusto281003@gmail.com>
2026-06-26 21:37:37 -0300

    add: added Spring packpage

---
+++ financehub-api/src/main/java/com/augustosouza/financehub/FinancehubApiApplication.java
@@ -0,0 +1,13 @@
+package com.augustosouza.financehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancehubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancehubApiApplication.class, args);
+	}
+
+}
---
+++ financehub-api/src/test/java/com/augustosouza/financehub/FinancehubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustosouza.financehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancehubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

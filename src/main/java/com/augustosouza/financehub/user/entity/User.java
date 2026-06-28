c2d5622483ba653e3780a5fd80730af0c2755bd6
Augusto Souza <augusto281003@gmail.com>
2026-06-28 17:29:09 -0300

    feat:chore: initialize Spring Boot project

--- src/main/java/com/augustosouza/financehub/user/entity/User.java
+++
@@ -1,8 +0,0 @@
-package com.augustosouza.financehub.user.entity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
-public class User {
-}
